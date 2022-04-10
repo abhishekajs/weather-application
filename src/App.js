b5d105af22bb8e2d2ec85482dbccbb6fbdb97888
Abhishek Singh <abhisheksingh0914@gmail.com>
2022-04-11 01:47:12 +0530

    Completed: Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import CityComponent from "./components/CityComponent";
+import WeatherComponent from "./components/WeatherComponent";
+
+const API_KEY = "de8ec7840ce97b7de2e18e5369c29261";
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    updateWeather(res.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <span id="Head-Label">The Weather App</span>
+      {weather ? (
+        <WeatherComponent weather={weather} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
     </div>
   );
 }
